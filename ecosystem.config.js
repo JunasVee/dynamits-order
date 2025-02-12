5598c19d23711c943ef1c0d8eda50b79fac53a27
perdanamain <firman.fp123@gmail.com>
2025-02-12 16:03:14 +0700

    deploy: set up pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "driver-app",
+      name: "fe-customer-app",
       script: "node_modules/next/dist/bin/next", // Points to Next.js start script
       args: "start", // Command to start Next.js in production mode
       instances: 1, // Use 'max' to scale based on CPU cores
@@ -10,11 +10,11 @@ module.exports = {
       max_memory_restart: "1G", // Restart if memory exceeds 1GB
       env: {
         NODE_ENV: "development",
-        PORT: 3000, // Customize the port for development
+        PORT: 3001, // Customize the port for development
       },
       env_production: {
         NODE_ENV: "production",
-        PORT: 3000, // Customize the port for production
+        PORT: 3001, // Customize the port for production
       },
     },
   ],
